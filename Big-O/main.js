d68344d076b4be1fe33ca8fa8ca75eb1928ecdd8
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-01-22 14:29:57 +0100

    Update Big-O

--- Big-O/main.js
+++ Big-O/main.js
@@ -1,16 +1,21 @@
+// Big-O = O(n)
 const nemo = ['nemo'];
 const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat',
 'nigel','squirt', 'darla', 'hank']
 const large = new Array(1000).fill('nemo')
 function findNemo (array) {
-    let t0 = performance.now();
     for (let i = 0; i < array.length; i++) {
         if (array[i] === 'nemo') {
             console.log('Found NEMO!!')
         }
     }
-    let t1 = performance.now();
-    console.log('Call to find nemo took ' + (t1 - t0));
 }
 
 findNemo(large);
+
+//Big-O = O(1)
+const boxes = [0,1,2,3,4,5]
+function logFirstTwoBoxes(boxes) {
+    console.log(boxes[0]);
+    console.log(boxes[1]);
+}
