5289782c505be7c4da82a3d8d1354dcbace64f83
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-01-25 14:37:07 +0100

    Updated React

--- React/main.jsx
+++ React/main.jsx
@@ -7,3 +7,44 @@ export default function App() {
         </div>
     )
 }
+
+export default function Greetings ({ message }) {
+    return (<h1>{`Hello ${message}`}</h1>)
+}
+
+// React State 
+
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+function User() {
+    const [message, setMessage] = useState('Welcome to react');
+
+    return (
+        <div>
+            <h1>{message}</h1>
+        </div>
+    )
+}
+
+const ChildComponent = (props) => {
+    return (
+        <div>
+            <p>{props.name}</p>
+            <p>{props.age}</p>
+        </div>
+    )
+}
+
+const ParentComponent = () => {
+    return (
+        <div>
+            <ChildComponent name = "Okiki" age = "25" />
+            <ChildComponent name = "Malomo" age = "27" />
+            <button onClick={activateLasers}></button>
+        </div>
+    )
+}
+
+setState({ name: "Daniel" }, () => {
+    console.log("The name has been updated ")
+})
