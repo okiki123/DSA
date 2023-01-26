81f02409c0313c2704c5d0ce4c6bfa75b528b6f3
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-01-26 14:46:04 +0100

    Updated Commit

--- examples.js
+++ examples.js
@@ -14,3 +14,22 @@ function containsCommonItems(arr1, arr2) {
 }
 
 console.log(containsCommonItems(array1, array2));
+
+function containsCommonItems2(arr1, arr2) {
+    let map = {};
+    for (let i = 0; i < arr1.length; i++) {
+        if(!map[arr1[i]]) {
+            const item = arr1[i];
+            map[item] = true;
+        }
+    }
+
+    for (let j = 0; j < arr2.length; j++) {
+        if (map[arr2[j]]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(containsCommonItems2(array1, array2));
