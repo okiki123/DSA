822c0d117d6e70b2c91c7a4febcf67e7a2666ddf
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-01-27 13:35:25 +0100

    Updated Commit

--- examples.js
+++ examples.js
@@ -69,3 +69,38 @@ class Wizard extends Player {
 const wizard1 = new Wizard('Shally', 'Healer');
 const wizard2 = new Wizard('Shawn', 'Dark Magic');
 console.log(wizard1.play())
+
+//Implementing an Array
+class myArray {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+    get(index) {
+        return this.data[index];
+    }
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
+    pop() {
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return lastItem;
+    }
+    delete(index) {
+        const item = this.data[index];
+        this.shiftItems(index);
+    }
+    shiftItems(index) {
+        for (let i = index; i < this.length -1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+    } 
+}
+
+const newArray = new myArray;
+newArray.push('1');
+console.log(newArray);
