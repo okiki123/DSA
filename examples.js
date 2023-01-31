c292a87aa37d95778bb7e6f8a8fc31b480682fc7
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-01-31 23:42:03 +0100

    Updated Commit

--- examples.js
+++ examples.js
@@ -117,3 +117,13 @@ function reverse(str) {
 
 }
 
+function reverse(str) {
+    //check input
+    if(!str || str.length < 2 || typeof str !== 'string') {
+        return 'Not reversible'
+    }
+    const backwards = []
+    return backwards.join();
+
+}
+
