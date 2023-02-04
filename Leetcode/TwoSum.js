0447a554b3e331ec367f4e913cc6fa2320a9ed56
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-04 22:46:38 +0100

    TwoSum Solution

--- Leetcode/TwoSum.js
+++ Leetcode/TwoSum.js
@@ -1,3 +1,9 @@
 var twoSum = function(nums, target) {
-    
-};
+     for (let i = 0; i < nums.length; i++) {
+         const compliment = target - nums[i];
+         if (compliment in map) {
+             return [map[compliment], i]
+         }
+         map[nums[i]] = i
+     }
+ };
