c76c8a02941cf4d6665e7be13fc043e690eed16f
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-05 10:56:29 +0100

    Added main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Alice has some cards with numbers written on them. 
+# She arranges the cards in decreasing order, and lays them out face down in a sequence on a table. 
+# She challenges Bob to pick out the card containing a given number by turning over as few cards as possible. 
+# Write a function to help Bob locate the card.
+
+
+def locate_number(card, query):
+    position = 0
+    while True:
+        if card[position] == query:
+            return position
+        position += 1
+        if len(card) == position:
+            return -1
+
