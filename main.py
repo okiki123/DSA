182ac216ea33cf313b03ff731d74ccb4130fbfa2
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-05 11:57:49 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -3,13 +3,28 @@
 # She challenges Bob to pick out the card containing a given number by turning over as few cards as possible. 
 # Write a function to help Bob locate the card.
 
-
-def locate_number(card, query):
+# Brute Force Approach
+def locate_position(cards, query):
     position = 0
-    while position < len(card):
-        if card[position] == query:
+    while position < len(cards):
+        if cards[position] == query:
             return position
         position += 1
+    return -1
+
+# Using Binary Search Algorithm
+def locate_position(cards, query):
+    lo = 0
+    hi = len(cards) - 1
+    while lo <= hi:
+        mid = (lo + hi) / 2
+        mid_number = cards[mid]
+        if mid_number == query:
+            return mid
+        elif mid_number < query:
+            hi = mid - 1
+        elif mid_number > query:
+            lo = mid + 1
     return -1
 
 
