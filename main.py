b639eb775d719e9460cb5c5ad816182e03c5e46b
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-08 14:53:53 +0100

    Updated Commit

--- main.py
+++ main.py
@@ -65,5 +65,68 @@ class LinkedList:
         if self.head is None:
             print("Linked list is empty")
             return
+        itr = self.head
+        llist = ''
+        while itr:
+            llist += str(itr.data) + '-->'
+            itr = itr.next
+        print(llist)
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(data, None)
+
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+        return count
+
+    def remove_at(self, index):
+        if index < 0 or index >= self.get_length:
+            raise Exception("Invalid index")
+
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+
+    def insert_at(self, index):
+        if index < 0 or index >= self.get_length():
+            raise Exception("Invalid index")
+        if index == 0:
+            self.insert_at_beginning(data)
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+            itr = itr.next
+            count += 1
 
 
+
+ 
