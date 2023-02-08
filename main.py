859d4cb1ddc1d9fbbf0d35842aee26fd01b635f6
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-08 09:31:10 +0100

    Updated Commit

--- main.py
+++ main.py
@@ -46,4 +46,24 @@ odd_number = []
 for i in range(1, max):
     if i % 2 == 1:
         odd_number.append(i)
-        
+
+# Linked  List
+class Node:
+    def __init__(self, data = None, next = None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert_at_beginning(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def print(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+
+
