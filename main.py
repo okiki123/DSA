b7cef918074b3def6ec1c447d8fa39a3a9ab9c4c
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-09 10:47:27 +0100

    Updated Commit

--- main.py
+++ main.py
@@ -199,5 +199,24 @@ class stack:
     def size(self):
         return len(self.container)
 
+# Queue
+class queue:
+    def __init__(self):
+        self.buffer = deque()
+    
+    def enqueue(self, val):
+        self.buffer.appendleft(val)
+        
+    def dequeue(self):
+        return self.buffer.pop()
+    
+    def is_empty(self):
+        return len(self.buffer)==0
+    
+    def size(self):
+        return len(self.buffer)
+
+
+
 
 
