288fb88913e218b8b7e0af80e343b7118e1eef8d
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-09 13:34:47 +0100

    Updated Commit

--- main.py
+++ main.py
@@ -228,6 +228,18 @@ class TreeNode:
         self.children.append(child)
 
 
+# Recursion
+def find_sum(n):
+    sum = 0
+    for i in range(1, n+1):
+        sum+=1
+    return sum
+
+def find_sum_recursive(n):
+    if n == 1:
+        return 1
+    return n + find_sum_recursive(n - 1)
+
 
 
 
