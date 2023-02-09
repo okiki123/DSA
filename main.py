7b584fd939defe2578841f96c13fdd7bbb0c2140
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-09 09:20:16 +0100

    Updated Commit

--- main.py
+++ main.py
@@ -136,4 +136,31 @@ class LinkedList:
             self.insert_at_end(data)
 
 
- 
+# Hash Table
+def get_hash(key):
+    h = 0
+    for char in key:
+        h += ord(char)
+    return h % 100
+
+class HashTable:
+    def __init__(self):
+        self.MAX = 100
+        self.arr = [None for i in range(self.MAX)]
+    def get_hash(self, key):
+        h = 0
+        for char in key:
+            h += ord(char)
+        return h % self.MAX
+    def set_item(self, key, val):
+        h = self.get_hash(key)
+        self.arr[h] = val
+
+    def get_item(self, key):
+        h = self.get_hash(key)
+        return self.arr[h]
+    def del_item(self, key):
+        h = self.get_hash(key)
+        self.arr[h] = None
+
+
