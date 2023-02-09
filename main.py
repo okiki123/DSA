8c48f98ebb72110223867758535e85c68e72d514
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-09 12:24:45 +0100

    Updated Commit

--- main.py
+++ main.py
@@ -216,6 +216,18 @@ class queue:
     def size(self):
         return len(self.buffer)
 
+#Tree
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        self.parent = None
+
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
+
+
 
 
 
