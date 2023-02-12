0f5cd69240373a2a4b415de28bef6e37f3097cbc
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-12 23:03:52 +0100

    Updated Commit

--- Leetcode/ReverseLinkedList.py
+++ Leetcode/ReverseLinkedList.py
@@ -3,8 +3,5 @@ class ListNode:
     def __init__(self, val):
         self.val = val
         self.next = None
-class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None:
-            return head
+
         
