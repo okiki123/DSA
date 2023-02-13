74a85d3ce2ecaa1194b2d0827e514c446564a7e9
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-13 14:26:51 +0100

    Updated Commit

--- Leetcode/ReverseLinkedList.py
+++ Leetcode/ReverseLinkedList.py
@@ -33,3 +33,20 @@ class  LinkedList:
             self.head.prev = new_node
 
         self.head = new_node
+    
+    def insertAfter(self, prev_node, new_data):
+        if prev_node is None:
+            print("This node doesnt exist")
+            return
+        new_node = Node(data = new_data)
+        new_node.next = prev_node.next
+
+        prev_node.next = new_node
+
+        new_node.prev = prev_node
+
+        if new_node.next is not None:
+            new_node.next.prev = new_node
+
+
+
