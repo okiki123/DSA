8522c8372f6158f952e95ebba1f1f89e5e473e8a
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-13 14:14:35 +0100

    Updated Commit

--- Leetcode/ReverseLinkedList.py
+++ Leetcode/ReverseLinkedList.py
@@ -20,3 +20,16 @@ class Node:
         self.data = data
         self.prev = None
         self.next = None
+class  LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(data = new_data)
+        new_node.next = self.head
+        new_node.prev = None
+        
+        if self.head is not None:
+            self.head.prev = new_node
+
+        self.head = new_node
