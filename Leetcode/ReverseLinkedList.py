2d059922ef19cabc11fdd8504c5eb9e810fb8734
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-13 13:58:07 +0100

    Updated Commit

--- Leetcode/ReverseLinkedList.py
+++ Leetcode/ReverseLinkedList.py
@@ -15,3 +15,8 @@ class LinkedList:
 
 
 #Doubly LinkedList
+class Node:
+    def __init__(self, data = None, prev = None, next = None):
+        self.data = data
+        self.prev = None
+        self.next = None
