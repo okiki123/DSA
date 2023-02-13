4bc12f2c61d42f6c3bc50375f98d6012b710c003
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-13 19:54:59 +0100

    Updated Commit

--- Leetcode/ReverseLinkedList.py
+++ Leetcode/ReverseLinkedList.py
@@ -1,52 +1,19 @@
 # Given the head of a singly linked list, reverse the list, and return the reversed list.
 class ListNode:
-    def __init__(self, val):
-        self.val = val
+    def __init__(self,  val):
+        self.val =  val
         self.next = None
-
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-
-#Doubly LinkedList
-class Node:
-    def __init__(self, data = None, prev = None, next = None):
-        self.data = data
-        self.prev = None
-        self.next = None
-class  LinkedList:
-    def __init__(self):
-        self.head = None
-
-    def push(self, new_data):
-        new_node = Node(data = new_data)
-        new_node.next = self.head
-        new_node.prev = None
+class Solution:
+    def reverseList(self, head: ListNode)  -> ListNode:
+        prev, curr = None, head
         
-        if self.head is not None:
-            self.head.prev = new_node
+        while curr:
+            nxt  = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
 
-        self.head = new_node
-    
-    def insertAfter(self, prev_node, new_data):
-        if prev_node is None:
-            print("This node doesnt exist")
-            return
-        new_node = Node(data = new_data)
-        new_node.next = prev_node.next
-
-        prev_node.next = new_node
-
-        new_node.prev = prev_node
-
-        if new_node.next is not None:
-            new_node.next.prev = new_node
 
 
 
