72afa12c9ea24ada58e3ec82997879824af9aa7a
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-17 15:04:18 +0100

    Updated Commit

--- DSA Patterns/Slidingwindow.py
+++ DSA Patterns/Slidingwindow.py
@@ -49,7 +49,25 @@ def smallest_subarray_with_given_sum(s, arr):
         return min_length
 
 def longest_substring_with_k_distinct(str, k):
-    window_start
+    window_start = 0
+    max_length  = 0
+    char_frequency = {}
+
+    for window_end in range(len(str)):
+        right_char = str[window_end]
+        if right_char not in char_frequency:
+            char_frequency[right_char] = 0
+        char_frequency[right_char] += 1
+
+        while len(char_frequency) > k:
+            left_char = str[window_start]
+            char_frequency[left_char] -= 1
+            if char_frequency[left_char] == 0:
+                del char_frequency[left_char]
+            window_start += 1
+        max_length  = max(max_length, window_end  - window_start + 1)
+    return max_length
+
         
 
 
