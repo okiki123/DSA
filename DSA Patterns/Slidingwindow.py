c910db261902ac5f55887bdd9d6ba6d80a7dd255
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-17 14:38:55 +0100

    Updated Commit

--- DSA Patterns/Slidingwindow.py
+++ DSA Patterns/Slidingwindow.py
@@ -33,4 +33,21 @@ def max_sub_array_of_size_k(k, arr):
             windowsum -= arr[windowstart]
             windowstart += 1
         return maxsum
+import math
+def smallest_subarray_with_given_sum(s, arr):
+    window_sum = 0
+    min_length  = math.inf
+    window_start = 0
+    for window_end in range(len(arr)):
+        window_sum += arr[window_end]
+        while window_sum >= s:
+            min_length = min(min_length, window_end - window_start + 1)
+            window_sum - arr[window_start]
+            window_start += 1
+        if min_length == math.inf:
+            return 0
+        return min_length
+        
+
+
 
