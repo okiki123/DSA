8c3f6b42d2eceaf37bff5ab706ed255ff0d86eba
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-18 11:01:36 +0100

    Updated Commit

--- DSA Patterns/Slidingwindow.py
+++ DSA Patterns/Slidingwindow.py
@@ -86,10 +86,10 @@ def remove_duplicates(arr):
     next_non_duplicate = 1
     i = 1
     while(i < len(arr)):
-        if arr[next_non_duplicate - 1] != arr[i]
-        arr[next_non_duplicate] = arr[i]
-        next_non_duplicate += 1
-    i += 1
+        if arr[next_non_duplicate - 1] != arr[i]:
+            arr[next_non_duplicate] = arr[i]
+            next_non_duplicate += 1
+        i += 1
     return next_non_duplicate
 
 def remove_element(arr, key):
