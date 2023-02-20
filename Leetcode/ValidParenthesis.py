a486966abd4e9fc3abb149ca355d14b10aa9afb8
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-20 22:20:09 +0100

    Updated Commit

--- Leetcode/ValidParenthesis.py
+++ Leetcode/ValidParenthesis.py
@@ -1,4 +1,12 @@
 class solution:
     def isValid(self, s:str) -> bool:
-        
+        stack = []
+        dict = {'(':')', '[':']', '{':'}'}
+        for i in s:
+            if i in dict:
+                stack.append(i)
+            elif len(stack) == 0 or dict[stack.pop()] != i:
+                return False
+        return len(stack) == 0
+
  
