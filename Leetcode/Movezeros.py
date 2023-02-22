70a55835b7d14e8ee5d4fdff0449e347994e6c40
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-22 19:15:42 +0100

    Updated Commit

--- Leetcode/Movezeros.py
+++ Leetcode/Movezeros.py
@@ -1,2 +1,8 @@
 class solution:
-    
+    def moveZeros(self, nums:list) -> list:
+        prev_idx = 0
+        for i in range(len(nums)):
+            hold = nums[prev_idx]
+            nums[prev_idx] = nums[i]
+            nums[i] = hold
+            prev_idx += 1
