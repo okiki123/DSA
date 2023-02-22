d155837383e643f6a3ff7b61570ad467583da210
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-22 12:54:07 +0100

    Updated Commit

--- Leetcode/FirstUniqueCharacterInString.py
+++ Leetcode/FirstUniqueCharacterInString.py
@@ -4,4 +4,11 @@ class solution:
     def firstUniqueChar(self, s: str) -> int:
         st_map = {}
         for i in range(len(s)):
-            
+            if s[i] not in st_map:
+                st_map[s[i]] = True
+            else:
+                st_map[s[i]] = False
+        for i in range(len(s)):
+            if st_map[s[i]] == True:
+                return i
+        return -1
