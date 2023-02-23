a4c428973dc7aa6b1e02884931bb489cc470f3ee
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-23 12:15:36 +0100

    Updated Commit

--- Leetcode/sqrt.py
+++ Leetcode/sqrt.py
@@ -1,3 +1,13 @@
 class solution:
     def sqrt(self, x: int) -> int:
-        
+        lo, hi = x
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if mid*mid > hi:
+                hi = mid - 1
+            elif mid*mid < hi:
+                lo = mid + 1
+            else:
+                return mid
+        return hi
+
