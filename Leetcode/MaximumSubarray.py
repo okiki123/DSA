5057496cd4256d4aefa403d1af0f6243d5f13bb0
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-23 15:21:48 +0100

    Updated Commit

--- Leetcode/MaximumSubarray.py
+++ Leetcode/MaximumSubarray.py
@@ -1,2 +1,10 @@
 class solution:
-    
+    def maxSubarray(self, nums:list[int]) -> int:
+        currSum = 0
+        maxSub = nums[0]
+        for n in nums:
+            if currSum < 0:
+                currSum = 0
+            currSum += n
+            maxSub = max(currSum, maxSub)
+        return maxSub
