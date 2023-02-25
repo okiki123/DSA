35d5bc4b3363e0ffb7a23e13f83ce4874bed4e29
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-25 09:20:06 +0100

    Updated Commit

--- Leetcode/TwoSum.py
+++ Leetcode/TwoSum.py
@@ -1,3 +1,10 @@
 class solution:
-    def twosum(self, nums:list) -> list:
+    def twosum(self, nums:list[int], target:int) -> list[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            x = target - nums[i]
+            if x in hashmap:
+                return [map[x], i]
+            else:
+                hashmap[nums[i]] = i
         
