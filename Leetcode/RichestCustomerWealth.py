8431630947d0058fea849fa1b46fce6989762526
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-02-28 14:18:02 +0100

    Updated Commit

--- Leetcode/RichestCustomerWealth.py
+++ Leetcode/RichestCustomerWealth.py
@@ -1,2 +1,9 @@
 class solution:
-    
+    def richestcustomer(self, accounts:List[List[int]]) -> int:
+        maxsum = 0
+        for i in range(len(accounts)):
+            eachsum = 0
+            for j in range(len(accounts[i])):
+                eachsum += accounts[i][j]
+                maxsum = max(maxsum, eachsum)
+        return maxsum
