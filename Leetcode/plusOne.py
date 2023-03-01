0a64b247ab0ea502a9cabfa4c0c80aaa933217e3
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-03-01 14:41:16 +0100

    Updated Commit

--- Leetcode/plusOne.py
+++ Leetcode/plusOne.py
@@ -1,2 +1,12 @@
 class solution:
     def plusone(self, digits: list[int]) -> int:
+        length = len(digits) - 1
+        while digits[length] == 9:
+            digits[length] = 0
+            length -= 1
+        if length < 0:
+            digits = [1] + digits
+        else:
+            digits[length] += 1
+        return digits
+    
