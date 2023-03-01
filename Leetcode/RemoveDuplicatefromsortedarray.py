45a130986cbfe61eb48ed2c4154f6d9c41df4bd1
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-03-01 12:12:35 +0100

    Updated Commit

--- Leetcode/RemoveDuplicatefromsortedarray.py
+++ Leetcode/RemoveDuplicatefromsortedarray.py
@@ -1,3 +1,8 @@
 class solution:
     def removeduplicatefromsortedarray(self, nums:list[int]) -> int:
-        
+        left = 1
+        for right in range(1, len(nums)):
+            if nums[right] != nums[right - 1]:
+                nums[left] = nums[right]
+                left += 1
+        return left
