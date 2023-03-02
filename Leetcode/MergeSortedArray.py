09bb51e0ad55daa748721b6b7ff823577130a312
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-03-02 15:17:05 +0100

    Updated Commit

--- Leetcode/MergeSortedArray.py
+++ Leetcode/MergeSortedArray.py
@@ -1,2 +1,6 @@
 class solution:
     def mergesortedarrays(self, nums1:list[int], m:int, nums2:list[int], n:int) -> list:
+        last = m + n - 1
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[last] = nums1[m - 1]
