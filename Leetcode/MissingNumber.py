3a21f53373551448a6527712c5cccffb46ee3dcf
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-03-10 21:20:13 +0100

    Updated Commit

--- Leetcode/MissingNumber.py
+++ Leetcode/MissingNumber.py
@@ -7,4 +7,11 @@ class solution:
                 nums[i], nums[j] = nums[j], nums[i]
             else:
                 i += 1
+        for i in range(n):
+            if nums[i] != i:
+                return i
+        return n
+
+
+
 
