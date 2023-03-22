6bfb352a69d5273d6f94840de93a42fc06cb1e2e
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-03-22 18:49:15 +0100

    Updated Commit

--- Leetcode/LinkedListcylec.py
+++ Leetcode/LinkedListcylec.py
@@ -1,3 +1,9 @@
 class solution:
     def linkedlistcycle(self, head: Optional[ListNode]) -> bool:
         slow, fast = head, head
+        while fast is not None and fast.next is not None:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
