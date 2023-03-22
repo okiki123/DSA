6dd2f1108cc5e2a78ed041058218772f38a50173
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-03-22 09:38:44 +0100

    Updated Commit

--- Leetcode/checkDuplicate.py
+++ Leetcode/checkDuplicate.py
@@ -1,3 +1,9 @@
 class solution:
     def containsDuplicate(self, nums:list[int]) -> bool:
-        
+        check = {}
+        for i in range(len(nums)):
+            if nums[i] not in check:
+                check[nums[i]] = i
+            else:
+                return True
+        return False
