cb461fc0138b31fe0fcabf063e7d2e37a64e7f3c
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-04-11 10:24:09 +0100

    Updated Commit

--- Leetcode/meetingRooms.py
+++ Leetcode/meetingRooms.py
@@ -1,3 +1,4 @@
 class solution:
-    def meetingrooms(self, intervals[list[list[int]]]) -> bool:
-        
+    def meetingrooms(self, intervals: [list[list[int]]]) -> bool:
+        intervals.sort(key=lambda x: x[0])
+        for i in range(len(intervals)- 1)
