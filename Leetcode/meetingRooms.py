8de6c4ef5ed3afc83eee3bcf95c6b2f56ec53f22
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-04-11 10:25:22 +0100

    Updated Commit

--- Leetcode/meetingRooms.py
+++ Leetcode/meetingRooms.py
@@ -1,4 +1,7 @@
 class solution:
     def meetingrooms(self, intervals: [list[list[int]]]) -> bool:
         intervals.sort(key=lambda x: x[0])
-        for i in range(len(intervals)- 1)
+        for i in range(len(intervals) - 1):
+            if intervals[i][1] > intervals[i + 1][0]:
+                return False
+        return True 
