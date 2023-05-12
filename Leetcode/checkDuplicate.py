650bb5cfda59efb56ea189eb88ee2f4b32d6abfd
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-05-12 20:46:31 +0100

    Updated Commit

--- Leetcode/checkDuplicate.py
+++ Leetcode/checkDuplicate.py
@@ -9,7 +9,7 @@ class solution:
         return False
     
     def containsDuplicate2(self, nums:list[int]) -> bool:
-        hashset = set()
+        map = set()
         for n in nums:
             if n in hashset:
                 return True
