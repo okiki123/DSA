6f2bc39714a60f588789200c58765441d2744a52
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-05-13 09:01:44 +0100

    Updated Commit

--- Leetcode/ValidAnagram.py
+++ Leetcode/ValidAnagram.py
@@ -1,3 +1,8 @@
 def isAnagram(self, s:str, t:str) -> bool:
+    countS, countT = {}, {}
     if len(s) != len(t):
         return False
+    for i in range(len(s)):
+        countS[s[i]] = 1 + countS.get(s[i], 0)
+        countS[t[i]] = 1 + countT.get(t[i], 0)
+    return countS == countT
