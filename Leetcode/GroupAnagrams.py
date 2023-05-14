5e10447bf62bf62663aecc8c9dcf6474517eee4b
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-05-14 20:49:59 +0100

    Updated Commit

--- Leetcode/GroupAnagrams.py
+++ Leetcode/GroupAnagrams.py
@@ -1,1 +1,8 @@
 def groupAnagrams(self, strs:list[str]) -> list[list[str]]:
+    res = defaultdict(list)
+    for c in strs:
+        count = [0] * 26
+        for a in c:
+            count[ord(a) - ord("a")] += 1
+        res[tuple[count]].append(c)
+    return res.value()
