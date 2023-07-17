9caabff82c6fb2f890c2f4163e64cd7be8f50d2e
Malomo Okikioluwa <okikimalomo17@gmail.com>
2023-07-17 12:16:26 +0100

    Updated Commit

--- Leetcode/productArrayExceptItself.py
+++ Leetcode/productArrayExceptItself.py
@@ -1,2 +1,9 @@
 class solution:
-    def productExceptSelf(self, nums:list[int]) -> list[int]
+    def productExceptSelf(self, nums:list[int]) -> list[int]:
+        sol = [1] * len(nums)
+        pre =  1
+        post = 1
+        for i in range(len(nums)):
+            sol[i] *= pre
+            pre *= nums[i]
+            
